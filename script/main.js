e99bc6b085b25a379a102bafd1a34567b6904135
Cherv1213 <vlad70052@gmail.com>
2023-02-17 17:07:46 +0200

    243

--- script/main.js
+++ script/main.js
@@ -1,14 +1,4 @@
-"use strict"
 
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.header__menu');
-if(iconMenu){
-   iconMenu.addEventListener("click", function (e){
-      document.body.classList.toggle('_lock');
-      iconMenu.classList.toggle('_active');
-      menuBody.classList.toggle('_active');
-   });
-}
 
 $('.open-overlay').click(function() {
    var overlay_navigation = $('.overlay-navigation'),
